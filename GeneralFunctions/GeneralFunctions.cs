e65b153cc500ad3480de324cf0489bfb73bfdd7a
Damien Sawyer <damiensawyer@gmail.com>
2017-05-24 13:37:45 +1000

    init

---
+++ GeneralFunctions/GeneralFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeneralFunctions
+{
+    public static class GeneralFunctions
+    {
+        public static int Add(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
+`
